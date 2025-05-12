eb503b3f9852c8c78e0d52c158491aba0c4eeb56
Muhammad Afif Faizun <afiffaizun31@gmail.com>
2025-05-12 08:10:27 +0700

    Update Code

---
+++ src/Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from './components/Navigation/Navigation.jsx'
+import HeroSection from './components/HeroSection/HeroSection.jsx';
+
+function Shop() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navigation />
+    <HeroSection />
+    </>
+  )
+}
+
+export default Shop
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Shop from './Shop'; 
+//import reportWebVitals from './reportWebVitals';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Shop />
+  </React.StrictMode>
+);
+
+reportWebVitals();
